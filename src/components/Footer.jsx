b681164f370d6c3842cfae7e9fa1f755b2bede32
Amir <vibelessyoung@gmail.com>
2026-02-06 19:21:29 +0330

    basic footer created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="w-full h-20 flex justify-center items-center bg-violet-900">
+      <h1 className="text-2xl text-white font-medium">footer</h1>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div className="w-full h-20 flex justify-center items-center bg-violet-900">
-    <h1 className="text-2xl text-white font-medium">
-        SHOP
-    </h1>
-  </div>;
+  return (
+    <div className="w-full h-20 flex justify-center items-center bg-violet-900">
+      <h1 className="text-2xl text-white font-medium">SHOP</h1>
+    </div>
+  );
 }
 
 export default Header;
