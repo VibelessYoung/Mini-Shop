d093d7dc1e7da340547725a7e9019086a4db456e
Amir <vibelessyoung@gmail.com>
2026-02-08 14:13:44 +0330

    add dynomic products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Filter from "./Filter";
 import Products from "./Products";
 import ShoppingCart from "./ShoppingCart";
 import Footer from "./Footer";
+import data from "../data.json";
 
 function Home() {
+  const [item, setItem] = useState(data.products);
+  console.log(item);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#24243e]">
       <Header />
@@ -13,7 +17,7 @@ function Home() {
 
       <main className="max-w-7xl mx-auto flex flex-col md:flex-row gap-6 px-4">
         <ShoppingCart />
-        <Products />
+        <Products item={item} />
       </main>
 
       <Footer />
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,23 +1,21 @@
-function Products() {
-  const products = Array.from({ length: 9 });
-
+function Products(props) {
   return (
     <section className="flex-1 p-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
-        {products.map((_, i) => (
+        {props.item.map((item) => (
           <div
-            key={i}
+            key={item.id}
             className="group bg-white rounded-3xl shadow-lg hover:shadow-2xl transition-all duration-300 hover:-translate-y-2"
           >
             <div className="h-44 bg-gradient-to-br from-gray-200 to-gray-300 rounded-t-3xl flex items-center justify-center text-gray-500 group-hover:scale-105 transition">
-              IMAGE
+              <img src={item.image} alt="" />
             </div>
 
             <div className="p-5 flex flex-col gap-3">
-              <h3 className="text-xl font-bold text-center">محصول {i + 1}</h3>
+              <h3 className="text-xl font-bold text-center">محصول {item.title}</h3>
 
               <p className="text-gray-500 text-sm text-center">
-                توضیح کوتاه و جذاب برای محصول
+                {item.price}
               </p>
 
               <button className="mt-4 bg-gradient-to-r from-violet-600 to-indigo-600 text-white py-2 rounded-xl font-semibold hover:opacity-90 transition">
