e4be2cad5951768269c7d893fca684951a333793
Amir <vibelessyoung@gmail.com>
2026-02-09 18:36:38 +0330

    render items to shoppingcart's ui

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,20 +1,20 @@
 function ShoppingCart(props) {
-  console.log(props);
+  const { cartItems } = props;
   return (
     <aside className="w-full md:w-80 p-6 top-24">
       <div className="bg-white rounded-3xl shadow-xl p-5 space-y-4">
         <h2 className="font-bold text-green-700 text-center">
-          🛒 3 آیتم در سبد خرید
+          🛒 {cartItems.length} آیتم در سبد خرید
         </h2>
 
-        {[1, 2, 3].map((item) => (
+        {cartItems.map((item) => (
           <div
-            key={item}
+            key={item.id}
             className="flex justify-between items-center bg-gray-100 rounded-xl p-3 hover:bg-gray-200 transition"
           >
             <div className="flex flex-col text-sm">
-              <span>1 عدد</span>
-              <span className="font-bold">200 تومان</span>
+              <span>{item.qty} تعداد</span>
+              <span className="font-bold">{item.price} تومان</span>
               <button className="text-red-500 text-xs hover:underline">
                 حذف
               </button>
@@ -22,7 +22,7 @@ function ShoppingCart(props) {
 
             <div className="flex items-center gap-3">
               <div className="w-12 h-12 bg-gray-300 rounded-lg" />
-              <p className="font-semibold">موبایل</p>
+              <p className="font-semibold">{item.title}</p>
             </div>
           </div>
         ))}
